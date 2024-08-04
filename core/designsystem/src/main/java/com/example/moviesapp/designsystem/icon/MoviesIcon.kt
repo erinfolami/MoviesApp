6dbc3e49988d4ff30922e26aae1f3660e9d0fea2
erinemma <emmanuelerinfolami2@gmail.com>
2024-08-04 09:33:14 +0100

    added string resource

--- core/designsystem/src/main/java/com/example/moviesapp/designsystem/icon/MoviesIcon.kt
+++ core/designsystem/src/main/java/com/example/moviesapp/designsystem/icon/MoviesIcon.kt
@@ -1,16 +1,22 @@
 package com.example.moviesapp.designsystem.icon
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Bolt
+import androidx.compose.material.icons.rounded.Bookmarks
+import androidx.compose.material.icons.rounded.Call
+import androidx.compose.material.icons.rounded.Grid3x3
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material.icons.rounded.Upcoming
 import com.example.moviesapp.designsystem.R
 
 
 /**
  * Movies icons. Material icons are [ImageVector]s, custom icons are drawable resource IDs.
  */
-//object MoviesIcon {
-//        val Upcoming = Icons.Rounded.Upcoming
-//        val Bookmarks = Icons.Rounded.Bookmarks
-//        val Grid3x3 = Icons.Rounded.Grid3x3
-//
-//}
+object MoviesIcon {
+        val Trending = Icons.Rounded.Bolt
+        val Upcoming = Icons.Rounded.Upcoming
+        val Search = Icons.Rounded.Search
+
+}
 
