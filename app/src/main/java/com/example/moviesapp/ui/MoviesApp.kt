af881c3a19f7e7a116976abf950fda88f4ed3bee
erinemma <emmanuelerinfolami2@gmail.com>
2024-08-07 10:26:22 +0100

    added app level themes and typography

---
+++ app/src/main/java/com/example/moviesapp/ui/MoviesApp.kt
@@ -0,0 +1,14 @@
+package com.example.moviesapp.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.moviesapp.designsystem.component.MoviesAppBackground
+
+
+@Composable
+fun MoviesApp(modifier: Modifier = Modifier){
+
+    MoviesAppBackground(modifier = modifier)  {
+
+    }
+}
--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -42,4 +42,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    api(libs.androidx.ui.tooling.preview)
+
 }
