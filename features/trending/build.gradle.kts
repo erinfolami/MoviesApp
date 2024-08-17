811d2ab9ad8fc1a634ee19dda96ef962d3cfc3ae
erinemma <emmanuelerinfolami2@gmail.com>
2024-08-17 17:21:04 +0100

    fixed dependency conflict

--- features/trending/build.gradle.kts
+++ features/trending/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.navigation.runtime.ktx)
-    implementation(libs.navigation.compose)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
